7e0a548e4e26f5997bcb43653d299be17c273964
omnandurkar <nandurkarom172@gmail.com>
2024-01-25 14:56:31 +0530

    Done with the advance routing

--- src/view/Posts/Posts.js
+++ src/view/Posts/Posts.js
@@ -8,6 +8,9 @@ function Posts(){
         <>
         <Navbar/>
         <h1 className='p-5 text-center'>Posts</h1>
+        
+      {/* const id,title,description,content,author,publishedDate = {post} */}
+
         {
             blogData.map((post, index)=>{
                 return <PreviewPostCard key={index} id={post.id} title={post.title} description={post.description} content={post.content} author={post.author} publishedDate={post.publishedDate} />
--- src/view/PreviewPostCard/PreviewPostCard.js
+++ src/view/PreviewPostCard/PreviewPostCard.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-function PreviewPostCard({id, title, author, description}){
-    return(
-        <div className='container m-5 shadow border rounded p-5'>
-        <div className='preview-post-card'>
-        <h2>{title}</h2>
-        <span>By{author}</span>
-        <p>{description}</p>
-        <Link to={`/post/read/${id}`}>Read More</Link>
-        </div>
+function PreviewPostCard({ id, title, author, description }) {
+    return (
+        <div className="container m-5 shadow border rounded p-5">
+            <div className="preview-post-card">
+                <h2>{title}</h2>
+                <span>By{author}</span>
+                <p>{description}</p>
+                <Link to={`/post/read/${id}`}>
+                    <button className='btn btn-primary ' >Read More</button>
+                </Link>
+            </div>
         </div>
     )
 }
